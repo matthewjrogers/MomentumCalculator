9dd20435879c76df731b084b1d437c9b41d5475e
Matt Rogers <33400922+matthewjrogers@users.noreply.github.com>
2022-07-09 09:50:23 -0400

    Update ArcheryMomentumKECalculator.py

--- ArcheryMomentumKECalculator.py
+++ ArcheryMomentumKECalculator.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     wgr = float(input("Enter weight in grains:\n"))
     fps = float(input("Enter speed in FPS:\n"))
-    #print(wgr, fps)
+    
     # energy in footpounds = weight in grains times the square of the velocity divided by 450800
     footPounds = (wgr * fps * fps)/450800
     slugFPS = (wgr * fps)/225400
